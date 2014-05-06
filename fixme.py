fb3c894107d154c25ebdd0424662a0275736f16b
mprat <michele.pratusevich@gmail.com>
2014-05-06 16:12:33 +0300

    how to convert slides to PDF

---
+++ fixme.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2013 - Damian Avila
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# A little snippet to fix @media print issue printing slides from IPython
+#-----------------------------------------------------------------------------
+ 
+import io
+
+nbname = raw_input('Type notebook name: ')
+notebook = nbname + '.ipynb'
+path = notebook[:-6] + '.slides.html'
+flag = u'@media print{*{text-shadow:none !important;color:#000 !important'
+ 
+with io.open(path, 'r') as in_file:
+    data = in_file.readlines()
+    for i, line in enumerate(data):
+        if line[:64] == flag:
+            data[i] = data[i].replace('color:#000 !important;', '')
+ 
+with io.open(path, 'w') as out_file:
+    out_file.writelines(data)
+ 
+print "You can now print your slides"
