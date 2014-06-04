d0084746a4fea0490ffd2cefe63ce9ee7bb73e44
mprat <michele.pratusevich@gmail.com>
2014-06-04 14:15:56 +0300

    made ugly slide PDFs and pretty document PDFs

--- printpdfslides.py
+++ printpdfslides.py
@@ -5,7 +5,7 @@ if len(sys.argv) != 2:
 	print "type the name of the notebook as the command-line argument"
 else:
 	notebook = sys.argv[1]
-	path = notebook[:-6] + '.slides.html'
+	path = notebook + '.slides.html'
 	flag = u'@media print{*{text-shadow:none !important;color:#000 !important'
 	 
 	with io.open(path, 'r') as in_file:
